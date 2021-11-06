a60bd42ce85b05598267aa8412d157025ebc914a
sumitgsh <sumitgsh19@gmail.com>
2021-11-06 21:07:38 +0530

    teach-add-stud

---
+++ teacher/main-sidebar.php
@@ -0,0 +1,68 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+  <!-- Brand Logo -->
+  <a href="index.php" class="brand-link">
+    <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <span class="brand-text font-weight-light">FeedBack Management</span>
+  </a>
+
+  <!-- Sidebar -->
+  <div class="sidebar">
+    <!-- Sidebar user panel (optional) -->
+    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+      <div class="image">
+        <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+      </div>
+      <div class="info">
+        <a href="#" class="d-block">Teacher</a>
+      </div>
+    </div>
+
+    <!-- SidebarSearch Form -->
+    <div class="form-inline">
+      <div class="input-group" data-widget="sidebar-search">
+        <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+        <div class="input-group-append">
+          <button class="btn btn-sidebar">
+            <i class="fas fa-search fa-fw"></i>
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <!-- Sidebar Menu -->
+    <nav class="mt-2">
+      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+  
+      <li class="nav-item">
+          <a href="logout.php" class="nav-link">
+            <i class="far fa-edit"></i>
+            <p>Issue Feedback</p>
+          </a>
+
+        </li>
+        <li class="nav-item">
+          <a href="logout.php" class="nav-link">
+            <i class="fas fa-comments"></i>
+            <p>Issued Feedback</p>
+          </a>
+
+        </li>
+        <li class="nav-item">
+          <a href="logout.php" class="nav-link">
+            <i class="fas fa-chart-bar"></i>
+            <p>Analyse </p>
+          </a>
+
+        </li>
+        <li class="nav-item">
+          <a href="logout.php" class="nav-link">
+            <i class="fas fa-sign-out-alt"></i>
+            <p>Logout</p>
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <!-- /.sidebar-menu -->
+  </div>
+  <!-- /.sidebar -->
+</aside>
